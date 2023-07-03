1ef3704a10820cd64c7749b1c4614ed026e05cf8
LordLizard <WarriorXKz@gmail.com>
2023-07-03 23:01:42 +0300

    add animation

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -3,24 +3,68 @@ package main;
 import inputs.KeyBoardInputs;
 import inputs.MouseInputs;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class GamePanel extends JPanel {
     private KeyBoardInputs keyboardInputs;
     private MouseInputs mouseInputs;
-    private int xDelta = 100;
-    private int yDelta = 100;
-//    private int frames = 0;
-//    private long lastCheck = 0;
+    private float xDelta = 100;
+    private float yDelta = 100;
+    private BufferedImage image;
+    private BufferedImage[][] animations;
+    private int animTick, animIndex, animSpeed = 15;
+
     public GamePanel(){
         keyboardInputs = new KeyBoardInputs(this);
         mouseInputs = new MouseInputs(this);
+
+        importImg();
+        loadAnimations();
+
+        setPanelSize();
         addKeyListener(keyboardInputs);
         addMouseListener(mouseInputs);
         addMouseMotionListener(mouseInputs);
+    }
+
+    private void loadAnimations() {
+        animations = new BufferedImage[1][5];
+        for (int j = 0; j < animations.length; j++) {
+            for (int i = 0; i < animations[j].length; i++) {
+                //TODO
+//            animations[j][i] = image.getSubimage(i*64,j*40,64,40);
+            }
+        }
+    }
+
+    private void importImg() {
+        InputStream is = getClass().getResourceAsStream("/withoutCloth_black.png");
+        System.out.println(is);
+        try {
+            assert is != null;
+            image = ImageIO.read(is);
+        } catch (IOException e){
+            e.printStackTrace();
+        } finally {
+            try {
+                assert is != null;
+                is.close();
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void setPanelSize() {
+        Dimension size = new Dimension(1280,800);
+        setMinimumSize(size);
+        setPreferredSize(size);
+        setMaximumSize(size);
     }
 
     public void changeXDelta(int value){
@@ -39,18 +83,26 @@ public class GamePanel extends JPanel {
 
     }
 
-    public void paintComponent(Graphics g){
-        ImageIcon imageIcon = new ImageIcon("H:/guap/BorzoiGame/src/withoutCloth_black.png");
-        super.paintComponent(g);
-        g.drawRect(100, 100, 150, 100);
-        g.drawImage(imageIcon.getImage() ,xDelta, yDelta, null);
-
-//        frames++;
-//        if(System.currentTimeMillis() - lastCheck >= 1000){
-//            lastCheck = System.currentTimeMillis();
-//            System.out.println("fps" + frames);
-//            frames = 0;
-//        }
-//        repaint();
+    private void updateAnimationTick() {
+        animTick++;
+        if(animTick >= animSpeed){
+            animTick = 0;
+            animIndex++;
+            if (animIndex >= 5){
+                animIndex = 0;
+            }
+        }
     }
+
+    public void paintComponent(Graphics g){
+//        ImageIcon imageIcon = new ImageIcon("H:/guap/BorzoiGame/src/res/withoutCloth_black.png");
+        super.paintComponent(g);
+//        g.drawImage(imageIcon.getImage() ,xDelta, yDelta, null);
+        updateAnimationTick();
+
+        g.drawImage(image, (int) xDelta, (int) yDelta, 256, 256, null);
+        g.drawImage(animations[0][animIndex], (int) xDelta, (int) yDelta, 256, 256, null);
+    }
+
+
 }
--- src/main/GameWindow.java
+++ src/main/GameWindow.java
@@ -8,10 +8,13 @@ public class GameWindow {
     public GameWindow(GamePanel gamePanel){
         jFrame = new JFrame();
 
-        jFrame.setSize(400, 400);
+//        jFrame.setSize(400, 400);
         jFrame.setDefaultCloseOperation(jFrame.EXIT_ON_CLOSE);
         jFrame.add(gamePanel);
-        jFrame.setLocationRelativeTo(null);
+        jFrame.setLocation(0, 0);
+//        jFrame.setLocationRelativeTo(null);
+        jFrame.setResizable(false);
+        jFrame.pack();
         jFrame.setVisible(true);
     }
 }
