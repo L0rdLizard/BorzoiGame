860389a9dc60f480510bf03c4189336408f4a7ed
LordLizard <WarriorXKz@gmail.com>
2023-07-16 20:33:24 +0300

    fullscreen game

--- src/main/Game.java
+++ src/main/Game.java
@@ -34,8 +34,10 @@ public class Game implements Runnable{
 
     public final static int TILES_DEFAULT_SIZE = 32;
     public final static float SCALE = 2f;
-    public final static int TILES_IN_WIDTH = 26;
-    public final static int TILES_IN_HEIGHT = 14;
+//    public final static int TILES_IN_WIDTH = 26;
+//    public final static int TILES_IN_HEIGHT = 14;
+    public final static int TILES_IN_WIDTH = 30;
+    public final static int TILES_IN_HEIGHT = 17;
     public final static int TILES_SIZE = (int) (TILES_DEFAULT_SIZE * SCALE);
     public static int GAME_WIDTH = TILES_SIZE * TILES_IN_WIDTH;
     public static int GAME_HEIGHT = TILES_SIZE * TILES_IN_HEIGHT;
--- src/main/GameWindow.java
+++ src/main/GameWindow.java
@@ -1,6 +1,7 @@
 package main;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowFocusListener;
 
@@ -9,14 +10,15 @@ public class GameWindow {
 
     public GameWindow(GamePanel gamePanel){
         jFrame = new JFrame();
-
-//        jFrame.setSize(400, 400);
+        jFrame.setUndecorated(true);
         jFrame.setDefaultCloseOperation(jFrame.EXIT_ON_CLOSE);
         jFrame.add(gamePanel);
 //        jFrame.setLocation(0, 0);
         jFrame.setResizable(false);
         jFrame.pack();
-        jFrame.setLocationRelativeTo(null);
+//        jFrame.setLocationRelativeTo(null);
+        jFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+//        jFrame.setSize(Toolkit.getDefaultToolkit().getScreenSize());
         jFrame.setVisible(true);
         jFrame.addWindowFocusListener(new WindowFocusListener() {
             @Override
